d317c582982b3c8d981181736d54b948ae57d0c0
Liudas Panavas <panavas.l@northeastern.edu>
2024-06-03 14:39:05 -0400

    dataset updates

--- pages/2_Dataset_View.py
+++ pages/2_Dataset_View.py
@@ -50,7 +50,7 @@ st.set_page_config(layout="wide")
 st.title('Dataset Selection')
 
 # Use the `tabs` function to create tabs
-tab1,  tab2 = st.tabs(["PreLoaded Datasets", "Synthetic Dataset"])
+tab1, tab2, tab3 = st.tabs(["PreLoaded Datasets", "Synthetic Dataset", "Upload Dataset"])
 
 @st.cache_data()
 def load_datasets(datasets):
@@ -111,54 +111,58 @@ with tab2:
         st.session_state['synthetic_data'] = {}
 
     col1, col2 = st.columns(2)
-   
 
     with col1:
-      
         col_name = st.text_input("Column Name:")
 
         if col_name:
-            data_type = st.selectbox("Data Type:", ["Numeric", "Categorical"])
+            if col_name in st.session_state['synthetic_data']:
+                st.error("Column name already exists. Please choose a different name.")
+            else:
+                try:
+                    data_type = st.selectbox("Data Type:", ["Numeric", "Categorical"])
 
-            if data_type == "Numeric":
-                data_format =  st.selectbox("Data Format:", ["Integer", "Float"])
-                distribution = st.selectbox("Distribution:", ["Normal", "Uniform", "Skewed"])
+                    if data_type == "Numeric":
+                        data_format = st.selectbox("Data Format:", ["Integer", "Float"])
+                        distribution = st.selectbox("Distribution:", ["Normal", "Uniform", "Skewed"])
 
-                low = st.number_input("Low:", value=-50)
-                high = st.number_input("High:", value=50)
+                        low = st.number_input("Low:", value=0)
+                        high = st.number_input("High:", value=50)
 
-                if distribution == "Normal":
-                    mean = st.slider("Mean:", low, high, (low + high) // 2)
-                    std = st.slider("Standard Deviation:", 0.1, (high - low) / 2, 1.0)
-                elif distribution == "Uniform":
-                    pass  # No additional parameters needed for uniform distribution
-                elif distribution == "Skewed":
-                    a = st.slider("Skew:", -10.0, 10.0, 0.0)
+                        if distribution == "Normal":
+                            mean = st.slider("Mean:", low, high, (low + high) // 2)
+                            std = st.slider("Standard Deviation:", 0.1, (high - low) / 2, 1.0)
+                        elif distribution == "Uniform":
+                            pass  # No additional parameters needed for uniform distribution
+                        elif distribution == "Skewed":
+                            a = st.slider("Skew:", -10.0, 10.0, 0.0)
 
-            elif data_type == "Categorical":
-                n_categories = st.slider("Number of Categories:", 2, 10, 3)
-                category_weights = [st.slider(f"Weight for Category {i+1}:", 1, 100, 25) for i in range(n_categories)]
+                    elif data_type == "Categorical":
+                        n_categories = st.slider("Number of Categories:", 2, 10, 3)
+                        category_weights = [st.slider(f"Weight for Category {i+1}:", 1, 100, 25) for i in range(n_categories)]
 
-            if data_type == "Numeric":
-                if distribution == "Normal":
-                    col_data = []
-                    while len(col_data) < st.session_state['n_records']:
-                        sample = norm.rvs(loc=mean, scale=std)
-                        if low <= sample <= high:
-                            if data_format == "Integer":
-                                sample = round(sample)  # Convert to integer
-                            col_data.append(sample)
-                elif distribution == "Uniform":
-                    col_data = randint.rvs(low, high+1, size=st.session_state['n_records'])
-                elif distribution == "Skewed":
-                    col_data = skewnorm.rvs(a,  scale=a, size=st.session_state['n_records'])
-                # clip data to specified range
-                # col_data = np.clip(col_data, low, high) 
-            elif data_type == "Categorical":
-                # normalize category weights
-                normalized_weights = [float(i)/sum(category_weights) for i in category_weights]
-                col_data = np.random.choice(range(n_categories), size=st.session_state['n_records'], p=normalized_weights)
-            st.session_state['synthetic_data'][col_name] = col_data
+                    if data_type == "Numeric":
+                        if distribution == "Normal":
+                            col_data = []
+                            while len(col_data) < st.session_state['n_records']:
+                                sample = norm.rvs(loc=mean, scale=std)
+                                if low <= sample <= high:
+                                    if data_format == "Integer":
+                                        sample = round(sample)  # Convert to integer
+                                    col_data.append(sample)
+                        elif distribution == "Uniform":
+                            col_data = randint.rvs(low, high+1, size=st.session_state['n_records'])
+                        elif distribution == "Skewed":
+                            col_data = skewnorm.rvs(a, scale=a, size=st.session_state['n_records'])
+                        # clip data to specified range
+                        # col_data = np.clip(col_data, low, high)
+                    elif data_type == "Categorical":
+                        # normalize category weights
+                        normalized_weights = [float(i)/sum(category_weights) for i in category_weights]
+                        col_data = np.random.choice(range(n_categories), size=st.session_state['n_records'], p=normalized_weights)
+                    st.session_state['synthetic_data'][col_name] = col_data
+                except ValueError:
+                    st.error("Invalid input. Please enter a valid number.")
 
     with col2:
         st.header("Distribution Preview")
@@ -171,22 +175,54 @@ with tab2:
             # Add a button to add the synthetic dataframe to the selected dataframe of session state
             if st.button("Use Synthetic Data"):
                 st.session_state.selected_dataset = df
-                switch_page("Query Selection")      
+                switch_page("Query Selection")
         
-# import pandas as pd
-
-# with tab2:
+# with tab3:
 #     st.header('Upload Dataset')
 #     st.write('Please upload a dataset with column names as the first row. DO NOT UPLOAD SENSITIVE DATA.')
 #     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-#     url = st.text_input('Or enter a URL')
 
 #     if uploaded_file:
-#         st.session_state['selected_dataset'] = pd.read_csv(uploaded_file)
-#     elif url:
 #         try:
-#             st.session_state['selected_dataset'] = pd.read_csv(url)
-#         except Exception as e:
-#             st.error(f"Error reading CSV from URL: {e}")
+#             df = pd.read_csv(uploaded_file)
+#             if df.columns.str.contains('Unnamed').any():
+#                 st.error("The uploaded file does not have headers as the top row.")
+#             elif df.isnull().values.any():
+#                 st.error("The uploaded file contains empty values.")
+#             elif not all(dtype.name in ['int64', 'float64', 'object'] for dtype in df.dtypes):
+#                 st.error("The uploaded file contains columns with unsupported data types.")
+#             else:
+#                 st.success("The uploaded file is valid.")
+#                 if st.button("Use Uploaded Data"):
+#                     st.session_state['selected_dataset'] = df
+#                     switch_page("Query Selection")
+#         except pd.errors.EmptyDataError:
+#             st.error("The uploaded file is empty.")
+#         except pd.errors.ParserError as e:
+#             st.error(f"Error parsing the uploaded file: {e}")
+with tab3:
+    st.header('Upload Dataset')
+    st.write('Please upload a dataset with column names as the first row. DO NOT UPLOAD SENSITIVE DATA.')
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
+    if uploaded_file:
+        try:
+            df = pd.read_csv(uploaded_file)
+            if df.columns.str.contains('Unnamed').any():
+                st.error("The uploaded file does not have headers as the top row.")
+            elif df.isnull().values.any():
+                null_columns = df.columns[df.isnull().any()]
+                st.error(f"The uploaded file contains empty values in the following columns: {', '.join(null_columns)}")
+            elif not all(dtype.name in ['int64', 'float64', 'object'] for dtype in df.dtypes):
+                unsupported_columns = df.columns[~df.dtypes.isin(['int64', 'float64', 'object'])]
+                st.error(f"The uploaded file contains columns with unsupported data types: {', '.join(unsupported_columns)}")
+            else:
+                st.success("The uploaded file is valid.")
+                if st.button("Go to Query Selection Page"):
+                    st.session_state['selected_dataset'] = df
+                    switch_page("Query Selection")
+        except pd.errors.EmptyDataError:
+            st.error("The uploaded file is empty.")
+        except pd.errors.ParserError as e:
+            st.error(f"Error parsing the uploaded file: {e}")
 st.session_state['active_page'] = 'Dataset_View'
